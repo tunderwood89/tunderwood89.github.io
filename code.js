5e425e3065f05e74c62b7b6ab0f35a10c4c01766
tunderwood89 <108191238+tunderwood89@users.noreply.github.com>
2023-04-09 16:08:51 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,14 @@
-var post= document.getElementById("post");
-post.addEventListener("click", function(){
-    var commentBoxValue= document.getElementById("comment-box").value;
- 
-    var li = document.createElement("li");
-    var text = document.createTextNode(commentBoxValue);
-    li.appendChild(text);
-    document.getElementById("unordered").appendChild(li);
- 
-});
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied!);
+}
